f5bb464a1ff0b6894fe8213e0ff78b6341b6e2fb
shlee2112 <shlee2112@gmail.com>
2024-04-05 04:58:34 -0500

    Add files via upload

--- js/navbar.js
+++ js/navbar.js
@@ -8,13 +8,14 @@ function replaceLogo() {
 
     if (w <= 992){
       // replace logo in mobile view
-      logoImg = '<h5><div class="logo-container" id="logo"><a href="#" class="logo_sm">Sanghyun Lee</a></div></h5>';
+      // logoImg = '<h5><div class="logo-container" id="logo"><a href="#" class="logo_sm">Sanghyun Lee</a></div></h5>';
+      logoImg = '<a href="index.html" class="logo">Sanghyun Lee</a>';
       uxAbr = 'UX';
       logo.innerHTML = logoImg;
       ux.innerHTML = uxAbr;
 
       // Add Mobile Nav
-      uxList = '<li><a href="heybunny.html">Heybunny</a></li><li><a href="google_internship.html">Google UX Design Internship</a></li><li><a href="docnetwork.html">DocNetwork</a></li><li><a href="ibm_maelstrom.html">IBM Design Maelstrom</a></li>';
+      uxList = '<li><a href="spotlight_pwd.html">Spotlight</a></li><li><a href="savia_design_system_pwd.html">Savia Design System</a></li><li><a href="heybunny.html">Heybunny</a></li>';
       hidden.innerHTML = uxList;
 
 
@@ -35,7 +36,7 @@ function replaceLogo() {
     let nav = document.getElementById("navbar");
 
     let links = nav.getElementsByTagName("a");
-    let uxlinks = ["heybunny.html", "google_internship.html", "docnetwork.html", "ibm_maelstrom.html"]
+    let uxlinks = ["spotlight_pwd.html", "savia_design_system_pwd.html", "heybunny.html", ]
     // console.log(links)
 
     for (i = 0; i < links.length; i++){
