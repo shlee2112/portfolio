9d5d16e31af90f8ae93ae8baab28d7f318af5358
shlee2112 <shlee2112@gmail.com>
2024-05-10 09:28:56 -0500

    Added new folder-inspire

---
+++ 646247ecf81f264267533e46e25d83aff203290c/js/navbar.js
@@ -0,0 +1,68 @@
+function replaceLogo() {
+    var w = window.outerWidth;
+    var logo = document.getElementById("logo");
+    var ux = document.getElementById("ux");
+    var hiddenList = document.getElementById("hidden");
+    var location = window.location.href;
+
+
+    uxList = '<li><a href="heybunny.html">Heybunny</a></li><li><a href="spotlight.html">Spotlight</a></li><li><a href="epp.html">E-Path Plus</a></li><li><a href="savia_design_system.html">Savia Design System</a></li>';
+
+
+    if (w <= 768){
+    hidden.innerHTML = '<li><a href="index.html">UX Home</a></li>' + uxList;
+    ux.href = "index.html"
+    ux.style.display = "none"
+  }
+  else{
+    hidden.innerHTML = '<li><a href="index.html">UX</a></li><li style="border-bottom: var(--border_color) 1px solid; width:372px;"></li>' + uxList;
+    ux.href = "#"
+    ux.style.display = "block"
+  }
+
+    
+    if (w <= 992){
+      // replace logo in mobile view
+      // logoImg = '<h5><div class="logo-container" id="logo"><a href="#" class="logo_sm">Sanghyun Lee</a></div></h5>';
+      logoImg = '<a href="index.html" class="logo">Sanghyun Lee</a>';
+      uxAbr = 'UX';
+      logo.innerHTML = logoImg;
+      ux.innerHTML = uxAbr;
+
+      
+
+    }
+    else{
+      logoImg = '<a href="index.html" class="logo">Sanghyun Lee</a>';
+      uxAbr = 'User Experience';
+      logo.innerHTML = logoImg;
+      ux.innerHTML = uxAbr;
+    }
+
+
+   // Current Location
+    let loc = location.split("/");
+    let currentLoc = loc[loc.length - 1];
+
+    let nav = document.getElementById("navbar");
+
+    let links = nav.getElementsByTagName("a");
+    let uxlinks = ["index.html", "heybunny.html", "spotlight.html", "savia_design_system.html", "epp.html", ]
+    // console.log(links)
+
+    for (i = 0; i < links.length; i++){
+      let linkhref = links[i].href.split("/");
+      let active = linkhref[linkhref.length - 1];      
+      // console.log(active)
+
+      if (active === currentLoc ){
+        if(links[i].text !== "Sanghyun Lee"){
+          links[i].classList.add("active");
+        }
+        if(uxlinks.includes(active)){
+          document.getElementById("ux").classList.add("active");
+        }
+      }
+    }
+
+}
